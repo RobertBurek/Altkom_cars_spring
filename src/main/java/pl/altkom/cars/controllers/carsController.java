c25cd7852c16f527ddab6032297d60731aed71e4
Robert Burek <robertburek@wp.pl>
2020-01-22 00:25:18 +0100

    pierwsza app

--- src/main/java/pl/altkom/cars/controllers/carsController.java
+++ src/main/java/pl/altkom/cars/controllers/carsController.java
@@ -3,10 +3,7 @@ package pl.altkom.cars.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import pl.altkom.cars.entities.Car;
 import pl.altkom.cars.repositories.CarsRepository;
 
