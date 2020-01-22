b14eb69108b8b4c98d6a78901cc06152020b04bf
Robert Burek <robertburek@wp.pl>
2020-01-22 23:07:27 +0100

    Nowa funkcjonalność - wyszukiwanie aut po marce

--- src/main/java/pl/altkom/cars/controllers/carsController.java
+++ src/main/java/pl/altkom/cars/controllers/carsController.java
@@ -3,9 +3,15 @@ package pl.altkom.cars.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.altkom.cars.entities.Car;
 import pl.altkom.cars.repositories.CarsRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class carsController {
@@ -16,6 +22,7 @@ public class carsController {
     @GetMapping("/cars")
     public String findAll(Model model) {
         model.addAttribute("carListModel", carsRepository.findAll());
+        model.addAttribute("carFind", new Car());
         return "carsView";
     }
 
@@ -30,6 +37,16 @@ public class carsController {
     public String deleteCar(@RequestParam Long carId, Model model) {
         carsRepository.deleteById(carId.longValue());
         return "redirect:/cars";
+    }
+
+    @PostMapping("/car/carFind")
+    public String findCar(@ModelAttribute Car carFind, Model model) {
+        List<Car> cars = (List<Car>) carsRepository.findAll();
+        cars = cars.stream()
+                .filter(car -> car.getBrand().equals(carFind.getBrand()))
+                .collect(Collectors.toList());
+        model.addAttribute("carListModel", cars);
+        return "carsViewFind";
     }
 
     @PostMapping("/car/save")
